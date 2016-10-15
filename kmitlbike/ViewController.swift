bcae3b2f93c87d6f07e38c281ec235a1899fb834
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2016-10-15 09:55:51 +0700

    Initial Commit

---
+++ kmitlbike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kmitlbike
+//
+//  Created by Kajornsak Peerapathananont on 10/15/2559 BE.
+//  Copyright © 2559 Kajornsak Peerapathananont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
